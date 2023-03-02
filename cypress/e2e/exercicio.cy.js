8fdec1cf792886253e874862d1c386b6c82f56d6
Lucas Oliveira <lucasoliveiracerrito@gmail.com>
2023-03-02 17:07:07 -0300

    Primeiro commit

---
+++ cypress/e2e/exercicio.cy.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+describe('Testes da Funcionalidade Usuários', () => {
+
+    beforeEach(() => {
+        
+    });
+
+    it('Deve validar contrato de usuários', () => {
+     
+    });
+
+    it('Deve listar usuários cadastrados', () => {
+         //TODO: 
+    });
+
+    it('Deve cadastrar um usuário com sucesso', () => {
+         //TODO: 
+    });
+
+    it('Deve validar um usuário com email inválido', () => {
+         //TODO: 
+    });
+
+    it('Deve editar um usuário previamente cadastrado', () => {
+         //TODO: 
+    });
+
+    it('Deve deletar um usuário previamente cadastrado', () => {
+        //TODO: 
+    });
+
+
+});
